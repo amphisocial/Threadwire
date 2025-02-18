14b86c2887f3e75108bea88933a57c391358c288
AkashArumugam6 <venkatachalapathy.a@northeastern.edu>
2025-02-18 16:59:40 -0500

    Updated landingPage css and authenticated routes

--- backend/routes/salesorders.js
+++ backend/routes/salesorders.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { getSalesOrders, editSalesOrder, importSalesOrders } = require('../controllers/salesordersController');
+const { authenticateToken } = require('../services/authToken')
+
 
 // Define endpoints
-router.get('/', getSalesOrders);
-router.post('/', editSalesOrder);
-router.post('/import',importSalesOrders);
+router.get('/', authenticateToken, getSalesOrders);
+router.post('/', authenticateToken, editSalesOrder);
+router.post('/import', authenticateToken,importSalesOrders);
 
 module.exports = router;
 
