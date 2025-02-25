6108757e9400f79d57d69061894025fee5d12556
EC2 Default User <ec2-user@ip-172-31-28-162.ec2.internal>
2025-02-25 21:20:30 +0000

    Saving Local changes

--- frontend/src/comps/Tabs.jsx
+++ frontend/src/comps/Tabs.jsx
@@ -44,7 +44,7 @@ const Tabs = ({ filters, onFilterChange }) => {
     <div className="tabs-container">
       <div className="top-row">
         <div className="tabs-group">
-          <div className="home-icon" onClick={() => navigate('/home')}>
+          <div className="home-iconn" onClick={() => navigate('/home')}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
               <polyline points="9 22 9 12 15 12 15 22"></polyline>
